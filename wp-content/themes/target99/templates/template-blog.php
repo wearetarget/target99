b04006534d87d0e2e95330ec43e3348c9b0d4059
rkarabanov <5600t0@gmail.com>
2017-07-27 21:54:45 +0300

    Add /about-us module

--- wp-content/themes/target99/templates/template-blog.php
+++ wp-content/themes/target99/templates/template-blog.php
@@ -6,43 +6,49 @@
 
 	// Post query arguments
 	$paged = ( get_query_var('paged') ) ? get_query_var('paged') : 1;
-	$post_args = [
-		'post_status'       => array('publish'),
-		'posts_per_page'    => 10,
-		'post_type'         => array( 'post'),
-		'order'             => 'DSC',
-		'orderby'           => 'date',
-		'paged'             => $paged
-	];
+// News request
+$news_args = [
+	'post_status' => array('publish'),
+	'post_type' => array('post'),
+	'category_name' => 'news',
+	'posts_per_page' => get_field('amount_of_news'),
+	'order' => 'DSC',
+	'orderby' => 'date',
+];
 
-	$the_query = new WP_Query($post_args);
+$news_query = new WP_Query($news_args);
 
 	get_header();
 ?>
 
 <section class="post-page">
 
-	<section class="post-page__posts-container">
-			<?php
-				if ( $the_query->have_posts() ) {
-					while ( $the_query->have_posts() ) {
-						$the_query->the_post();
-						$post_image_url = wp_get_attachment_image_src( get_post_thumbnail_id(), 'medium')[0];
-						$content_width_class = (bool)$post_image_url ? '' : 'news-post-single__details--full';
+	<?php if ($news_query->have_posts()) : ?>
+        <section class="short-news-panel">
+            <div class="short-news-panel__inner-container layout__content">
+                <div class="short-news-panel__title-container">
+                    <h2 class="short-news-panel__title">Новости</h2>
+                </div>
+
+                <div class="short-news-panel__list">
+					<?php
+					while ($news_query->have_posts()) {
+						$news_query->the_post();
+						$news_image_url = wp_get_attachment_image_src(get_post_thumbnail_id(), 'medium')[0];
 
 						include('template-parts/template-part-news-short.php');
 					}
 
 					wp_reset_postdata();
-				} else {
-			?>
-				<p><?php _e('There haven\'t been any posts yet. Check back soon!'); ?></p>
-			<?php } ?>
+					?>
+                </div>
 
-	</section>
-
-	<div class="post-page__pagination-container">
-	</div>
+                <div class="short-news-panel__footer">
+                    <span class="button button--warning button--mobile-full">Смотреть всю информацию</span>
+                </div>
+            </div>
+        </section>
+	<?php endif; ?>
 
 </section>
 
