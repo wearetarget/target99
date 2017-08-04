fba1971db172e2b4ce2893f4fea5e72828adc855
Konstantin Gontarev <kostia.gontarev@gmail.com>
2017-08-04 17:53:27 +0300

    Added promo post type. Implemented basics of the promo templates. Added one promo as example.

--- wp-content/themes/target99/templates/template-promo.php
+++ wp-content/themes/target99/templates/template-promo.php
@@ -4,47 +4,49 @@
 		Template Name: Promo
 	*/
 
-	// Post query arguments
 	$paged = ( get_query_var('paged') ) ? get_query_var('paged') : 1;
-	$post_args = [
-		'post_status'       => array('publish'),
-		'posts_per_page'    => 10,
-		'post_type'         => array( 'post'),
-		'order'             => 'DSC',
-		'orderby'           => 'date',
-		'paged'             => $paged
+
+	$promo_args = [
+		'post_status' => array('publish'),
+		'post_type' => array('promo'),
+		'order' => 'ASC',
+		'orderby' => 'date',
+		'posts_per_page' => 9,
+		'paged' => $paged
 	];
 
-	$the_query = new WP_Query($post_args);
+	$promo_query = new WP_Query($promo_args);
 
 	get_header();
 ?>
 
-<section class="post-page">
+<?php if ($promo_query->have_posts()) : ?>
+<section class="promo-page">
+	<div class="promo-page__inner-container layout__content">
+		<div class="promo-page__title-container">
+			<h2 class="promo-page__title">Информация об отходах</h2>
+		</div>
 
-	<section class="post-page__posts-container">
-			<?php
-				if ( $the_query->have_posts() ) {
-					while ( $the_query->have_posts() ) {
-						$the_query->the_post();
-						$post_image_url = wp_get_attachment_image_src( get_post_thumbnail_id(), 'medium')[0];
-						$content_width_class = (bool)$post_image_url ? '' : 'news-post-single__details--full';
+		<div class="promo-page__preview-container"></div>
 
-						include('template-parts/template-part-news-short.php');
+		<div class="promo-page__promos-list">
+			<div clss="promo-page__promo-item">
+				<?php
+					while ($promo_query->have_posts()) {
+						$promo_query->the_post();
+						$promo_preview = wp_get_attachment_image_src(get_post_thumbnail_id(), 'full')[0];
+
+						include('template-parts/template-part-promo.php');
 					}
 
 					wp_reset_postdata();
-				} else {
-			?>
-				<p><?php _e('There haven\'t been any posts yet. Check back soon!'); ?></p>
-			<?php } ?>
-
-	</section>
-
-	<div class="post-page__pagination-container">
+				?>
+			</div>
+		</div>
 	</div>
-
 </section>
+<?php endif; ?>
+
 
 <?php get_footer(); ?>
 
